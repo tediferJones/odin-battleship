d48b2f09b7a8502a04571fa9461579585b22198b
ted <theo_drz@live.com>
2022-05-14 14:56:03 -0400

    basic setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+console.log('hello');
+
+const idk = document.createElement('div');
+console.log(idk);
+idk.textContent = 'idk';
+document.querySelector('body').appendChild(idk);
